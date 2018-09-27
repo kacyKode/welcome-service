8a0678e09d24633a71e9a9a09ccd1eb9ff989fc2
kacyKode <smith.kacian.18@gmail.com>
2018-09-27 14:10:14 -0500

    Added Driver

---
+++ src/jm/edu/utech/ap/week5/Driver.java
@@ -0,0 +1,11 @@
+package jm.edu.utech.ap.week5;
+
+public class Driver implements Driveable{
+
+	@Override
+	public void drive(float numberofMiles) {
+		System.out.println(String.format("Kacy has driven" + "%.2f miles", numberofMiles));
+		
+	}
+
+}
